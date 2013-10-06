c40354c69a7cb33465a90b0f6e0218cc016a4e76
Patrik McKiernan <patrik@mckiernan.com>
2013-10-07 01:11:10 +0200

    Added bogus nop line.

--- GuildBrowser/Character.m
+++ GuildBrowser/Character.m
@@ -30,6 +30,7 @@
         // find selected spec
         _selectedSpec = [self selectedSpecFromTalents:data[@"talents"]];
         
+        int i=0;
         _neckItem = [Item initWithData:data[@"items"][@"neck"]];
         _wristItem = [Item initWithData:data[@"items"][@"wrist"]];
         _waistItem = [Item initWithData:data[@"items"][@"waist"]];
